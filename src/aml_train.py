33b61fed0438e7eedd8c76251dd9c870fc6b55c0
Karol Zak <karol.zak@hotmail.com>
2019-09-25 17:35:02 +0200

    feat implemented aml wrapper for train script

--- src/aml_train.py
+++ src/aml_train.py
@@ -1,5 +1,6 @@
 import argparse
 from azureml.core import Workspace, Dataset, Experiment, Run
+from azureml.core.model import Model
 # local imports
 from train import run_train
 
@@ -8,9 +9,17 @@ if __name__ == "__main__":
     parser.add_argument('--output_path', type=str, required=True)
     parser.add_argument('--input_path', type=str, required=True)
     args = parser.parse_args()
-
+    model_name = 'diabetes_model'
     run = Run.get_context()
-    print(run_train(args.input_path, args.output_path))
 
+    best_model_path, best_rmse, best_alpha = run_train(args.input_path, args.output_path)
+
+    run.log('rmse', best_rmse)
+    run.log('alpha', best_alpha)
+
+    run.upload_file(name='diabetes_model.pkl', path_or_stream=best_model_path)
+    # TODO register model if better than previous models?
+    if not 'offline' in run.id.lower():
+        run.register_model(model_name, best_model_path)
     run.complete()
 
