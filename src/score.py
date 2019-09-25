9fc6044a9f8f71599fbfd26fd2c00d5c33876956
Karol Zak <karol.zak@hotmail.com>
2019-09-25 16:33:34 +0200

    FEAT implemented scoring script

--- src/score.py
+++ src/score.py
@@ -1,4 +1,29 @@
-def run_score(input_path, output_path):
-    print(input_path)
-    print(output_path)
-    return "done scoring"
+import os
+import pandas as pd
+import datetime as dt
+from sklearn.externals import joblib
+
+
+def run_score(input_path, output_path, model_path, runid):
+
+    feat_cols = ['var1','var2','var3','var4','var5','var6','var7','var8','var9','var10']
+    
+    # load the data
+    df = pd.read_csv(input_path)
+    X = df[feat_cols].values
+    #y = df[label_col].values    
+    
+    # load the model
+    model = joblib.load(model_path)
+
+    # run scoring
+    y_pred = model.predict(X=X)
+    df['level'] = y_pred
+    df['runid'] = runid
+    df['timestamp'] = dt.datetime.now()
+    
+    os.makedirs(output_path, exist_ok=True)    
+    results_path = os.path.join(output_path, 'score_results.csv')
+    df.to_csv(results_path, index=False)
+
+    return results_path
