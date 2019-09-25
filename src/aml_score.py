08df47fcd2e5fd7098586dee978bc4b863dd1e6c
Karol Żak <karol.zak@hotmail.com>
2019-09-25 17:42:55 +0200

    implemented aml wrappers for train and score (#3)
    * added empty base files and structure
    
    * removed aml_wrappers folder
    
    * feat implemented aml wrapper for train script
    
    * feat implemented aml wrapper for score script

--- src/aml_score.py
+++ src/aml_score.py
@@ -1,5 +1,6 @@
 import argparse
 from azureml.core import Workspace, Dataset, Experiment, Run
+from azureml.core.model import Model
 # local imports
 from score import run_score
 
@@ -10,7 +11,15 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     run = Run.get_context()
-    print(run_score(args.input_path, args.output_path))
+
+    model_name = 'diabetes_model'
+    if 'offline' in run.id.lower():
+        model_path = 'models\model_alpha_best.pkl'
+    else:
+        model_path = Model.get_model_path(model_name=model_name)
+
+    results_path = run_score(args.input_path, args.output_path, model_path, run.id)
+    run.upload_file(name='score_results.csv', path_or_stream=results_path)
 
     run.complete()
 
