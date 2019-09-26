21c08b1fc9e2a7ef2f19c9b734d746e98516e316
Karol Zak <karol.zak@hotmail.com>
2019-09-27 00:06:45 +0200

    feat config usage for score/train scripts

--- src/score.py
+++ src/score.py
@@ -2,15 +2,14 @@ import os
 import pandas as pd
 import datetime as dt
 from sklearn.externals import joblib
+import config as cfg
 
 
 def run_score(input_path, output_path, model_path, runid):
-
-    feat_cols = ['var1','var2','var3','var4','var5','var6','var7','var8','var9','var10']
     
     # load the data
     df = pd.read_csv(input_path)
-    X = df[feat_cols].values
+    X = df[cfg.feat_cols].values
     #y = df[label_col].values    
     
     # load the model
--- src/train.py
+++ src/train.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.externals import joblib
+import config as cfg
 
 
 def train_step(X, y, alpha, model_path):
@@ -18,28 +19,18 @@ def train_step(X, y, alpha, model_path):
 def run_train(input_path, output_path):
     os.makedirs(output_path, exist_ok=True)
 
-    # set parameters
-    alphas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    model_name_base = 'model_alpha_%s.pkl'
-
-    feat_cols = ['var1','var2','var3','var4','var5','var6','var7','var8','var9','var10']
-    label_col = 'level'
-
-    test_size = 0.2
-    random_state = 66
-
     # read data from CSV file
     df = pd.read_csv(input_path)
-    X = df[feat_cols].values
-    y = df[label_col].values    
+    X = df[cfg.feat_cols].values
+    y = df[cfg.label_col].values    
 
     # model tunning
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=cfg.test_size, random_state=cfg.random_state)
     rmses = []
     model_paths = []
 
-    for alpha in alphas:
-        model_path = os.path.join(output_path, model_name_base % alpha)
+    for alpha in cfg.alphas:
+        model_path = os.path.join(output_path, cfg.model_name_base % alpha)
         model = train_step(X_train, y_train, alpha, model_path)
         y_pred = model.predict(X=X_test)
 
@@ -49,9 +40,9 @@ def run_train(input_path, output_path):
     # eval models
     # select the model with lowest mse
     best_model_index = np.argmin(rmses)
-    best_alpha = alphas[best_model_index]
+    best_alpha = cfg.alphas[best_model_index]
     best_rmse = rmses[best_model_index]
-    best_model_path = os.path.join(output_path, model_name_base % 'best')
+    best_model_path = os.path.join(output_path, cfg.model_name_base % 'best')
 
     # train final model with the whole dataset
     final_model = train_step(X, y, best_alpha, best_model_path)
