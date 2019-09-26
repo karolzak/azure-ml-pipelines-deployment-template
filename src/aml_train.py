0625b0b3bc935d1ba77f20b63bb39ecbf9aece83
Karol Zak <karol.zak@hotmail.com>
2019-09-27 00:05:23 +0200

    feat implemented config usage for aml wrappers

--- src/aml_train.py
+++ src/aml_train.py
@@ -3,14 +3,15 @@ from azureml.core import Workspace, Dataset, Experiment, Run
 from azureml.core.model import Model
 # local imports
 from train import run_train
+import config as cfg
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--output_path', type=str, required=True)
     parser.add_argument('--input_path', type=str, required=True)
     args = parser.parse_args()
-    model_name = 'diabetes_model'
     run = Run.get_context()
+    
 
     best_model_path, best_rmse, best_alpha = run_train(args.input_path, args.output_path)
 
@@ -20,6 +21,6 @@ if __name__ == "__main__":
     run.upload_file(name='diabetes_model.pkl', path_or_stream=best_model_path)
     # TODO register model if better than previous models?
     if not 'offline' in run.id.lower():
-        run.register_model(model_name, best_model_path)
+        run.register_model(cfg.model_name, best_model_path)
     run.complete()
 
